561f3ad790abdf960bedcef4199c781d19b8270b
sujeetku123 <102528893+sujeetku123@users.noreply.github.com>
2022-04-26 13:06:35 +0530

    Add files via upload

---
+++ ado Net linq entity a1/WebForm1.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Japan" />
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Win" />
+        <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Draw" />
+        <p>
+            &nbsp;</p>
+    </form>
+</body>
+</html>
---
+++ ado Net linq entity a1/WebForm1.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GetData();
+        }
+        private void GetData()
+        {
+            DataClasses1DataContext mydata = new DataClasses1DataContext();
+            GridView1.DataSource = mydata.FootBalls;
+            GridView1.DataBind();
+        }
+
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            japandata();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            windata();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            Drawdata();
+        }
+        private void japandata()
+        {
+            DataClasses1DataContext data = new DataClasses1DataContext();
+
+            GridView1.DataSource = from football in data.FootBalls
+                                   where football.TeamName1 == "Japan" || football.TeamName2 == "Japan"
+                                   select football;
+            GridView1.DataBind();
+        }
+        private void windata()
+        {
+            DataClasses1DataContext data = new DataClasses1DataContext();
+
+            GridView1.DataSource = from football in data.FootBalls
+                                   where football.Matchresult == "Win"
+                                   select football;
+            GridView1.DataBind();
+        }
+        private void Drawdata()
+        {
+            DataClasses1DataContext data = new DataClasses1DataContext();
+
+            GridView1.DataSource = from football in data.FootBalls
+                                   where football.Matchresult == "Draw"
+                                   select football;
+            GridView1.DataBind();
+        }
+
+    }
+}
